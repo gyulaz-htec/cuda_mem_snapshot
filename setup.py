94b2028f72ec56a6c9986f0a7726ad84471509f9
Gyula Zakor <Gyula.Zakor@amd.com>
2025-05-19 12:47:28 +0000

    Remove pytorch dependency

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='cuda_mem_snapshot',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'torch>=2.4.0'  # Specify the required PyTorch version
-    ],
     description='A small library providing CUDA memory management utilities.',
     author='Gyula Zakor',
     author_email='gyula.zakor@htecgroup.com',
